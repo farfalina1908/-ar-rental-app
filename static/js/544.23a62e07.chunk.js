1a10da3a0465892251c901bad982afca4a8e5cc7
farfalina1908 <110851966+farfalina1908@users.noreply.github.com>
2024-02-22 16:11:33 +0000

    Deploying to gh-pages from @ farfalina1908/car-rental-app@abf7b90fc67c08a09a1b3a4f5e486ccc50e489af 🚀

--- static/js/544.23a62e07.chunk.js
+++
@@ -1,2 +0,0 @@
-"use strict";(self.webpackChunkreact_homework_template=self.webpackChunkreact_homework_template||[]).push([[544],{544:function(e,t,r){r.r(t),r.d(t,{default:function(){return u}});var n=r(184);function u(){return(0,n.jsx)("h1",{children:"HELLO"})}}}]);
-//# sourceMappingURL=544.23a62e07.chunk.js.map
